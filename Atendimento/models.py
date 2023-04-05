3a01eb6ff48df771ca575206f1fdb0a7aa75595a
gustavohikaru <84883545+gustavohikaru@users.noreply.github.com>
2023-04-05 00:06:03 -0300

    correção do bd

--- Atendimento/models.py
+++ Atendimento/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from Aluno.models import Aluno
-from Aluno.models import Psicologa
+from Aluno.models import aluno
+from Psicologa.models import psicologa
 
 
 class Atendimento(models.Model):
     diaatendimento=models.DateField(max_length=12, null= True)
-    matriculaaluno=models.ForeignKey(Aluno, on_delete=models.CASCADE)
-    SUAPPsicologa=models.ForeignKey(Psicologa, on_delete=models.CASCADE)
+    matriculaaluno=models.ForeignKey(aluno, on_delete=models.CASCADE)
+    SUAPPsicologa=models.ForeignKey(psicologa, on_delete=models.CASCADE)
     observacoes = models.CharField(max_length=300)
     
